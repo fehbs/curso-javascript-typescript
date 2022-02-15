c9cf02097fb9078258d059d1fd3d5d96c62e4c6b
Fernando HBS <fernandobailao@gmail.com>
2022-02-15 09:07:36 -0300

    Add files via upload

---
+++ aula-13-Exercícios de JS-Luiz O/Exercicio const e let/index.js
@@ -0,0 +1,59 @@
+/* Gabrile Castella Bade tem 33 anos, pesa 80 kg
+tem 1.89 de altura e seu IMC é de xxxxxxxxxx
+Gabriele Bade nasceu em xxxxx
+IMC Formula ->     peso / (altura * altura)
+Ano de nascimento Formula ->   ano atual - idade
+*/
+/*
+const nome = 'Gabriele Castella';
+const sobrenome = 'Bade';
+const idade = 33;
+const peso = 80;
+const alturaEmM = 1.89;
+
+let indiceMassaCorporal = peso / (alturaEmM * alturaEmM);
+let anoNascimento = 2022 - 33;
+
+console.log(`${nome} ${sobrenome} tem ${idade} anos, pesa ${peso} kg,`);
+console.log(`tem ${alturaEmM} de altura e seu IMC é de ${indiceMassaCorporal}, `);
+console.log(`${nome} nasceu em ${anoNascimento}.`);   */
+
+/*José Dorival da Silva tem 60 anos, pesa 90 kg
+tem 1.67 de altura e seu IMC é de xxxxxxx
+José Dorival nasceu em xxxxxx.  */
+
+const nome = 'José Dorival';
+const sobrenome = 'Silva';
+const idade = 60;
+const peso = 90;
+const alturaEmMetro = 1.67;
+
+let imc = peso / (alturaEmMetro * alturaEmMetro);
+let anoNascimento = 2022 - idade;
+
+console.log(`${nome} ${sobrenome} tem ${idade} anos, pesa ${90} kg.`);
+console.log(`Tem ${alturaEmMetro} de altura e seu IMC é de ${imc}.`);
+console.log(`${nome} nasceu em ${anoNascimento}.`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
