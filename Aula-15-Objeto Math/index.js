8507690303701545cfb6820872e2f8fa4d9668b2
Fernando HBS <fernandobailao@gmail.com>
2022-02-17 17:44:59 -0300

    Aula 15. Objeto Math,

---
+++ Aula-15-Objeto Math/index.js
@@ -0,0 +1,43 @@
+// Objeto Math
+
+// Em JavaScript pode-se dividir um número por zero.
+// console.log(!!(100 / 0));
+
+/*let num1 = 16;
+console.log(num1 ** 0.5);*/  // formula para raiz-quadrada
+
+/*let num1 = 9.54578; 
+let num2 = Math.floor(num1);  // arredonda para baixo
+console.log(num2);*/
+
+/*let num1 = 9.54578;
+let num2 = Math.ceil(num1);  // arredonda para cima
+console.log(num2);*/
+
+/*let num1 = 9.54578;
+let num2 = Math.round(num1);
+console.log(num2);*/  // arredonda para o número mais próximo
+
+/*let num1 = 9.49;
+console.log(Math.max(1,2,3,4,5, -10,-50,1500,9,8,76));*/ // imprime o maior número
+
+//console.log(Math.min(1,2,3,4,5,-10,-50,1500,9,8,7,6)); // imprime 0 menor número
+
+/*const aleatorio = Math.round(Math.random() * (10 - 5) + 5);
+console.log(aleatorio);  // gera números aleatótios entre 0 e 1, mas, o número 1 não é incluído*/
+
+/*console.log(Math.pow(2, 10));
+console.log(2 ** 10);*/
+
+/* Fiquem a vontade Guys!!!
+     Bons estudos!!!
+       Power Up */
+       
+
+
+
+
+
+
+
+
