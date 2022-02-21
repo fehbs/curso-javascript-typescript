19983cc78657a8c1666f47bf7c1e06bfd514eeea
Fernando HBS <fernandobailao@gmail.com>
2022-02-21 17:03:10 -0300

    Add one lesson.

---
+++ Aula-17-Funcoes-(básico)/script.js
@@ -0,0 +1,164 @@
+// Functions Lesson //
+/* ex: 0 
+function saudacao() {
+    console.log('Bom dia!');
+}
+
+saudacao();
+
+function criarFuncao() {
+    console.log('Criando uma função em JavaScript of the Power!!!')
+}
+
+criarFuncao();
+
+function cumprimento() {
+    console.log('Olá GUY!')
+}
+
+cumprimento();
+
+function todayIs() {
+console.log('Today is Monday!!!')   
+}
+
+todayIs();
+
+function tomorrowIs() {
+    console.log('Tomorrow is Tuesday.')
+}
+
+tomorrowIs();
+
+function yesterdayWas() {
+    console.log('Yesterday was sunday.')
+}
+
+yesterdayWas();
+
+function declaracao() {
+    console.log('Eu amo JavaScript!!!')
+}
+
+declaracao(); */
+
+// ####################################### //
+
+// ####################################### //
+/* ex: 1 
+
+function saulutation(name) {
+    console.log(`Good Morning ${name}!.`);
+}
+
+saulutation('John Green');
+saulutation('Mary Black')
+saulutation('Morgan Flex') */
+
+// ####################################### //
+
+// ####################################### //
+/* ex: 2
+
+function saultation(name) {
+    return `Good Morning ${name} !.`;
+}
+
+const variable = saultation('#Gandolf#');
+console.log(variable); */
+
+// ####################################### //
+
+// ####################################### //
+// ex: 3 //
+
+/*function soma(x, y) {
+    const result = x + y;
+    return result;
+}
+
+console.log(soma(2, 2));
+console.log(soma(12.50, 14));
+console.log(soma(500, 400));
+console.log(soma(2.50, 2.50));
+console.log(soma(2.5, 2.5)); 
+
+
+/*function divisao(a, b) {
+    const resultado = a / b;
+    return resultado;
+}
+
+console.log(divisao(81, 9));
+console.log(divisao(25, 5));
+console.log(divisao(10, 2)); 
+
+function subtracao(a, b) {
+    const resultado = a - b;
+    return resultado;
+}
+
+console.log(subtracao(1000, 100));
+console.log(subtracao(100, 900));
+console.log(subtracao(-800, 100)); 
+
+function multiplicacao(x, y) {
+    const resultado = x * y;
+    return resultado;
+}
+
+console.log(multiplicacao(2, 5));
+console.log(multiplicacao(3, 7));
+console.log(multiplicacao(5, 5)); */
+
+// ####################################### //
+
+// ####################################### //
+/* ex: 4  // Função Anônima
+
+const raiz = function (n) {
+  return n ** 0.5;
+};                         // Nesse caso é preciso utilizar ponto e vírgula.
+
+console.log(raiz(9));
+console.log(raiz(81));
+console.log(raiz(25)); */
+
+// ####################################### //
+
+// ####################################### //
+// ex: 5  // Função Moderna
+
+//ex: (1 de 5) //
+/*
+const raiz = (n) => {
+    return n ** 0.5;
+};
+
+console.log(raiz(9)); */
+
+//ex: (2 de 5) //
+/*
+const raiz = n => n ** 0.5;
+
+console.log(raiz(81));
+console.log(raiz(16));
+console.log(raiz(25)); */
+
+// ########################## // 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
