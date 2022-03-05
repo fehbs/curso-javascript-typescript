369627dc405855075bb2c4490b2091f01d046976
Fernando HBS <fernandobailao@gmail.com>
2022-03-05 08:47:17 -0300

    Atribuicão via desestruturação/Arrays.

---
+++ Aula-29-Atribuição-via-desestruturaçãoArrays/main.js
@@ -0,0 +1,53 @@
+/* Ex(1*) /
+/*const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const primeiroNumero = numeros[0];
+console.log(primeiroNumero);*/
+
+/*const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9,];
+const firstNumber = numbers[0];
+console.log(firstNumber);*/
+
+/*const number = [1, 2, 3, 4, 5,];
+const secondNumber = number[2];
+console.log(secondNumber);*/
+
+// Ex(2) //
+/*
+const numeros = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000];
+const [primeiroNumero, segundoNumero, terceiroNumero] = numeros;
+console.log(segundoNumero - primeiroNumero);
+console.log(terceiroNumero, primeiroNumero);
+console.log(typeof(terceiroNumero, primeiroNumero));
+console.log(primeiroNumero);*/
+
+// Ex:(3) //
+/*
+const numeros = [100, 200, 300, 400, 500, 600, 700, 800];
+const [primeiroNumero, segundoNumero, ...resto] = numeros;  // ...resto = imprime o resto //
+console.log(primeiroNumero, segundoNumero);
+console.log(resto);*/
+
+// Ex:(4) //
+// ... spread => espalha os dados
+/*const numeros = [100, 200, 300, 400, 500, 600, 700, 800];
+const [um, , três, , cinco, , sete] = numeros;  
+console.log(um, três, cinco);*/
+
+
+// Ex:(5) //
+//                   0          1          2
+//                0  1  2    0  1  2    0  1  2
+/*const numeros = [ [1, 2, 3], [4, 5, 6], [7, 8, 9] ];
+const [lista1, lista2, lista3] = numeros;  
+console.log(typeof(lista3, lista1));
+console.log(lista1[1], lista2[2]);
+console.log(lista3);*/
+
+
+
+
+
+
+
+
+
