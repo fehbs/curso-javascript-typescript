472f6d4985b3219360eee4fb06112bd0073e76a7
Fernando HBS <fernandobailao@gmail.com>
2022-03-07 15:56:31 -0300

    Aula31ForClássico.

---
+++ Aula-31-forClássicoEstruturaDeRepetição/main.js
@@ -0,0 +1,58 @@
+ /*console.log('Linha 0');
+ console.log('Linha 1');
+ console.log('Linha 2');
+ console.log('Linha 3');
+ console.log('Linha 4');
+ console.log('Linha 5');*/
+
+/*
+ for (let i = 0; i <= 5; i++) {
+     console.log(i);
+ }
+
+ for (let i = 0; i <= 5; i++) {
+     console.log(`Linha ${i}`);
+ }*/
+
+ /*for (let i = 0; i <= 500; i++) {
+     console.log(`Índice ${i}`);
+ }*/
+
+ /*for (let i = 200; i <= 500; i++) {
+     console.log(`Vou começar à partir de 200 até chegar em 500: ${i}`);
+ }*/
+
+ /*for (let i = 400; i <= 500; i+= 10) {
+     console.log(`Vou pular de 10 em 10 até chegar em 500: ${i}`);
+ }*/
+
+ /*for (let i = -300; i <= 500; i += 50) {
+     console.log(`Linha ${i}`);
+ }*/
+
+ /*for (let i = 200; i >= 0; i -= 20) {
+     console.log(`Agora em ordem decrescente: ${i}`);
+ }*/
+
+ /*for (let i = 0; i <= 10; i++) {
+     const par = i % 2 === 0;
+     console.log(i, par);
+ }*/
+
+ /*for (let i = 0; i <= 10; i++) {
+     const par = i % 2 === 0 ? 'par' : 'ímpar';
+     console.log(i, par);
+ }*/
+
+/* const frutas =['Maça', 'Pêra', 'Uva'];
+
+ for (let i = 0; i < frutas.length; i++) {
+     console.log(i);
+ }*/
+
+ const frutas = ['Maça', 'Pêra', 'Uva', 'Caju', 'Manga', 'Laranja', 'Abacate'];
+
+ for (let i = 0; i < frutas.length; i++) {
+     console.log(`Índice ${i}`, frutas[i] );
+ }
+     
