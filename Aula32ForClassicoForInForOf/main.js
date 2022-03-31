49708ae9de229d4ff7fea9c969a2c67e8c361fee
Fernando HBS <fernandobailao@gmail.com>
2022-03-31 14:20:48 -0300

    Aula 31 for clássico for in for of

---
+++ Aula32ForClassicoForInForOf/main.js
@@ -0,0 +1,38 @@
+/*  For clássico - Geralmente com iteráveis (array ou strings)
+    For in - Retorna o indíce ou chave (string, array ou objetos)
+    For of - Retorna o valor em si (iteráveis, arrays ou strings)
+*/
+
+//const nomes = ['Fernando', 'HBS', 'Gabi'];//
+
+/*for (let i = 0; i < nomes.length; i++) {  //for clássico//
+    console.log(nomes[i]);
+}
+
+console.log('############');
+
+for (let i = 0; i < nomes.length; i++) {  //for i //
+    console.log(nomes[i]);
+};
+
+console.log('############');
+
+for (let valor of nomes) {   //for of //
+    console.log(valor);
+};
+
+console.log('############');
+
+nomes.forEach(function(valor,indice, array) {
+    console.log(valor, indice, array);
+});*/
+
+
+/*const pessoa = {  // objeto //
+    nome: 'Fernando',
+    sobrenome: 'HBS'
+};
+
+for (let chave in pessoa) {
+    console.log(chave, pessoa[chave]);
+}*/
