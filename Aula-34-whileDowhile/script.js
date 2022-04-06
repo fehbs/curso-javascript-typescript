ebddb9b53df1b1cf8cced7b1baca946dff4b38a7
Fernando HBS <fernandobailao@gmail.com>
2022-04-06 11:25:47 -0300

    while e  (do while)

---
+++ Aula-34-whileDowhile/script.js
@@ -0,0 +1,37 @@
+function random(min, max) {
+    const r = Math.random() * (max - min) + min;
+    return Math.floor(r);
+}
+
+const min = 1;
+const max = 50;
+let rand = 10;
+
+while (rand !== 10) {
+    rand = random(min, max);
+    console.log(rand);
+}
+
+console.log('######');
+
+do {                             //do => ele checa o código primeiro e depois a condição//
+    rand = random(min, max);
+    console.log(rand);
+} while(rand !== 10);
+
+
+/*function random(min, max) {
+    const r = Math.random() * (max - min);
+    return Math.floor(r);
+}
+
+let rand = random(1, 50);
+console.log(rand);*/
+
+
+/*let i = 0;
+
+while (i <= 10) {
+    console.log(i);
+    i++;
+}*/
