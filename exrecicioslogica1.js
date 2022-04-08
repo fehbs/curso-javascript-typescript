15928d879806bb0681b3983d9332d441cfc12fb8
Fernando HBS <fernandobailao@gmail.com>
2022-04-08 14:33:24 -0300

    ExercícciosLogicaJS_1_2_3

---
+++ exrecicioslogica1.js
@@ -0,0 +1,51 @@
+
+/*function max(x, y) {
+  if (x > y) {
+    return x;
+  } else {
+    return y;
+  }
+}
+
+console.log(max(2, 2));*/
+
+
+/*function majorValue(a, b) {
+  if (a >= b) {
+    return a;
+  } else
+  return b;
+}
+
+console.log(majorValue(4, 4));*/
+
+/*function max(x, y) {
+  if (x > y) return x;
+  return y;
+}
+
+console.log(max(10, 2));*/
+
+/*function max(x, y) {
+  return x > y ? x : y;
+}
+
+console.log(max(2, 3));*/
+
+/*function min(a, b) {
+  return a < b ? a : b;
+}
+
+console.log(min(5, 50));*/
+
+/*const max = (a, b) => {
+  return a > b ? a : b;
+}
+
+console.log(max(20, 10));*/
+
+/*const max = (x, y) => x > y ? x : y;
+console.log(max(10, 5));
+
+const min = (x, y) => x < y ? x : y; 
+console.log(min(200, 100));*/
---
+++ exrecicioslogica2_3.js
@@ -0,0 +1,45 @@
+/* Escreva uma função chamada ePaisagem que recebe
+dois argumentos, largura e altura de uma imagem (number).
+Retorne true se a imagem estiver no modo paisagem.
+*/
+
+
+/*function ePaisagem(largura, altura) {
+    return largura > altura ? true : false;
+}
+
+console.log(ePaisagem(1920, 1080));*/
+
+
+
+/*const ePaisagem = (largura, altura) => largura < altura;
+
+console.log(ePaisagem(100, 200));*/
+
+
+
+/* Escreva uma função que recebe um número e retorne o seguinte:
+Número é divisível por 3 = Fizz
+Número é divisível por 5 = Buzz
+Número é divisível por 3 e 5 = FizzBuzz
+Número não é divisível por 3 e 5 = Retorna o prórprio número
+Checar se o número é realmente um número
+Use a função com números de 0 a 100
+*/
+
+/*
+function fizzBuzz(numero) {
+    if (numero % 3 === 0 && numero % 5 === 0) return 'FizzBuzz';
+    if (typeof numero !== 'number') return numero;
+    if (numero % 3 === 0) return 'Fizz';
+    if (numero % 5 === 0) return 'Buzz';
+   
+    return numero
+}*/
+
+/*for (let i = 0; i <= 100; i++) {
+    console.log(fizzBuzz(i));
+}*/
+
+/*console.log(fizzBuzz(15));*/
+
