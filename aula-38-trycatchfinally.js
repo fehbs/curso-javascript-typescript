f111e8ecf3ac49c6463a8a453c94f03d4a5bfcfa
Fernando HBS <fernandobailao@gmail.com>
2022-04-10 13:52:53 -0300

    Tratando e lançando erros: try-catch-finally

---
+++ aula-38-trycatchfinally.js
@@ -0,0 +1,41 @@
+/*try {
+    // É executada quando não há erros
+} catch () {
+    // É executada quando há erros
+} finally {
+    // Sempre
+}*/
+/*
+try {
+    console.log(a);
+    console.log('Abri um arquivo');
+    console.log('Manipulei o arquivo e gerou erro');
+    console.log('Fechei o arquivo');    
+} catch (e) {
+    console.log('Tratando o erro');    
+} finally {
+    console.log('FINALLY: Eu sempre sou executado')
+} */
+
+/*
+try {
+    console.log('Abri um arquivo');
+    console.log('Manipulei o arquivo e gerou erro');
+    console.log('Fechei o arquivo');
+    
+    try {
+        console.log(b);
+    } catch(e) {
+        console.log('Deu erro');
+    } finally {
+        console.log('Também sou finally.')
+    }
+
+} catch (e) {
+    console.log('Tratando o erro');    
+} finally {
+    console.log('FINALLY: Eu sempre sou executado')
+} */
+
+
+
